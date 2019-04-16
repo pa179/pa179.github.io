bd7c087d76feff247f2b9beb534f471bbb9f7363
pa179 <48999971+pa179@users.noreply.github.com>
2019-04-16 12:18:18 +0200

    Add files via upload

--- Wasserwaage/service-worker.js
+++ Wasserwaage/service-worker.js
@@ -16,3 +16,45 @@ self.addEventListener("install", event => {
         })
    );
 });
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+
+                // Version #1: Cache all (!) responses and return error page
+                // Clone the request
+                let fetchRequest = event.request.clone();
+
+                return fetch(fetchRequest)
+                    .then(response => {
+                        // Check response
+                        if (!response || response.status !== 200 || response.type !== 'basic') {
+                            return response;
+                        }
+
+                        // Clone the response
+                        let responseToCache = response.clone();
+
+                        caches.open(CACHE_VERSION)
+                            .then(cache => {
+                                cache.put(event.request, responseToCache);
+                            });
+
+                        return response;
+                    })
+                    .catch(() => {
+                        return caches.match(OFFLINE_URL);
+                    });
+
+                // Version #2: Return error page
+                return fetch(event.request)
+                    .catch(() => {
+                        return caches.match(OFFLINE_URL);
+                    });
+            })
+    );
+});
