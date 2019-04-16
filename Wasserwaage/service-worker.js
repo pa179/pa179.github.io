32ccbb7e8e7b2bb3f3d49389ea4520faf6bee048
pa179 <48999971+pa179@users.noreply.github.com>
2019-04-16 12:17:04 +0200

    Add files via upload

--- Wasserwaage/service-worker.js
+++ Wasserwaage/service-worker.js
@@ -1,4 +1,18 @@
-// Listen for install event, set callback
-self.addEventListener('install', function(event) {
-    // Perform some task
+const VERSION = "1.0";
+const CACHE_VERSION = "pww" + VERSION;
+
+let cacheablePages = [
+    "/Wasserwaage/index.html"
+];
+
+// Pre-Cache all cacheable pags
+self.addEventListener("install", event => {
+    event.waitUntil(caches.open(CACHE_VERSION)
+        .then(cache => {
+            return cache.addAll(cacheablePages);
+        })
+        .then(() => {
+            return self.skipWaiting();
+        })
+   );
 });
