ead590d1d576475656dd778f24c3ffab8cbd8946
pa179 <48999971+pa179@users.noreply.github.com>
2019-04-16 12:20:36 +0200

    Add files via upload

--- Wasserwaage/service-worker.js
+++ Wasserwaage/service-worker.js
@@ -17,6 +17,22 @@ self.addEventListener("install", event => {
    );
 });
 
+// Cleanup old cache storages
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys()
+            .then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (CACHE_VERSION !== cacheName) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
 self.addEventListener("fetch", (event) => {
     event.respondWith(
         caches.match(event.request)
